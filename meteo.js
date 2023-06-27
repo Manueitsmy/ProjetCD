bf6ea78bdfe8495c824a37bb7dd96009e6a05589
manueitsmy <manueitsmy@gmail.com>
2023-06-28 00:14:10 +0200

    ok

---
+++ meteo.js
@@ -0,0 +1,28 @@
+function RecupereMeteo(){
+	let url = 'https://api.openweathermap.org/data/2.5/weather?q='+dataprops.param.VILLE+'&units=metric&lang=fr&appid='+dataprops.param.APIKEY;
+	//fetch(url).then((response) =>response.json().then((data) => console.log(data)));
+	fetch(url).then((response) =>response.json().then((data) => {
+		console.log(data);
+		document.querySelector('#city').innerHTML = data.name + '<BR> <img src="https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png" alt="'+data.weather[0].description+'">';
+		document.querySelector('#temp').innerHTML = data.weather[0].description;
+		document.querySelector('#humidity').innerHTML = data.name;
+		document.querySelector('#wind').innerHTML = data.name;
+
+		})
+	);
+}
+
+var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+        dataprops = JSON.parse(xmlhttp.responseText);
+			console.log(dataprops.param.VILLE);
+			//dataprops 
+        RecupereMeteo();
+    }
+  };
+
+  xmlhttp.open("GET","conf.json",true);
+  xmlhttp.send();
+	
